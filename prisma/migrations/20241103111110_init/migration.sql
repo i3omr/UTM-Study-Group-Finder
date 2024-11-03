b85b342a1ec980a2f13b92a49e39264609d484cf
Omar Elashry <rezk@graduate.utm.my>
2024-11-03 15:17:14 +0400

    Prisma

---
+++ prisma/migrations/20241103111110_init/migration.sql
@@ -0,0 +1,58 @@
+-- CreateEnum
+CREATE TYPE "ROLEenum" AS ENUM ('student', 'admin');
+
+-- CreateEnum
+CREATE TYPE "MAJORenum" AS ENUM ('ComputerScienceSoftwareEngineering', 'ComputerScienceNetworkAndSecurity');
+
+-- CreateEnum
+CREATE TYPE "COURSEenum" AS ENUM ('ArtificialIntelligence', 'Network');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "phone" TEXT,
+    "major" "MAJORenum" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "role" "ROLEenum" NOT NULL DEFAULT 'student',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Group" (
+    "id" TEXT NOT NULL,
+    "course" "COURSEenum" NOT NULL,
+    "description" TEXT,
+    "name" TEXT NOT NULL,
+    "groupDate" TIMESTAMP(3) NOT NULL,
+    "singleTime" BOOLEAN NOT NULL,
+    "major" "MAJORenum" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Group_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_GroupMembers" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_GroupMembers_AB_unique" ON "_GroupMembers"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_GroupMembers_B_index" ON "_GroupMembers"("B");
+
+-- AddForeignKey
+ALTER TABLE "_GroupMembers" ADD CONSTRAINT "_GroupMembers_A_fkey" FOREIGN KEY ("A") REFERENCES "Group"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_GroupMembers" ADD CONSTRAINT "_GroupMembers_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ prisma/migrations/20241103111445_added_new_major/migration.sql
@@ -0,0 +1,2 @@
+-- AlterEnum
+ALTER TYPE "MAJORenum" ADD VALUE 'ComputerScienceMultiMedia';
